cbecd813492be83482b059199af85a9cac63d21c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2024-07-17 20:46:46 +0530

    Time: 215 ms (50.04%), Space: 140.8 MB (45.71%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
+    typedef pair<int, pair<int, int>> P;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        typedef pair<int, pair<int, int>> P;
-        // can also be declared as priority_queue<P, vector<P>> pq;
-        priority_queue<P> pq;
         int n1=nums1.size();
         int n2=nums2.size();
+        // declaring min heap
+        priority_queue<P, vector<P>, greater<P>> pq;
+        // set to check if already visited or not
+        set<pair<int, int>>visited;
+        pq.push({nums1[0]+nums2[0],{0,0}});
+        visited.insert({0,0});
 
-        for(int i=0; i<n1; i++){
-            for(int j=0; j<n2; j++){
-                int sum=nums1[i]+nums2[j];
-
-                if(pq.size()<k){
-                    pq.push({sum,{i,j}});
-                }else if(sum<pq.top().first){
-                    pq.pop();
-                    pq.push({sum,{i,j}});
-                }else{
-                    break;// very important as it reduces the time complexity
-                }
-            }
-        }
-
+        // vector to store the ans
         vector<vector<int>> ans;
-        while(!pq.empty()){
+
+        while(k-- && !pq.empty()){
             auto top=pq.top();
             pq.pop();
             int i=top.second.first;
             int j=top.second.second;
-            ans.push_back({nums1[i], nums2[j]});
-        }
 
+            ans.push_back({nums1[i], nums2[j]});
+
+            if(j+1<n2 && visited.find({i, j+1})==visited.end()){
+                pq.push({nums1[i]+nums2[j+1],{i,j+1}});
+                visited.insert({i,j+1});
+            }
+
+
+             if(i+1<n1 && visited.find({i+1, j})==visited.end()){
+                pq.push({nums1[i+1]+nums2[j],{i+1,j}});
+                visited.insert({i+1, j});
+            }
+
+
+
+        }
         return ans;
+
         
     }
 };
