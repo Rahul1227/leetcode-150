6ff60049797de95e49f414424990d06ec8e92be2
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2024-07-25 22:59:31 +0530

    Time: 8 ms (33.9%), Space: 8.9 MB (51.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
 
     bool isvalid(char c){
-        return isalpha(c)||isdigit(c);
+        if((c>='a' && c<='z')||( c>='A' && c<='Z')|| (c>='0' && c<='9')) return true;
+        return false;
     }
 
 
