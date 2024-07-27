9d22b4557ac8e1ae94fdb0b2372cb974a945468d
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2024-07-27 10:40:00 +0530

    Time: 11 ms (52.88%), Space: 23.6 MB (11.2%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public: 
+    //calculating the leftmax
     vector<int> getLeftMax(vector<int>&height, int &n){
         vector<int> leftmax(n);
+        //assigning the first element of the height as the leftmax
         leftmax[0]=height[0];
         for(int i=1; i<n; i++){
             leftmax[i]=max(leftmax[i-1],height[i]);
@@ -11,6 +13,7 @@ public:
 
     vector<int> getRightMax(vector<int> &height, int&n){
         vector<int> rightmax(n);
+        //assigning the last element as the rightmax
         rightmax[n-1]=height[n-1];
         for(int i=n-2; i>=0; i--){
             rightmax[i]=max(rightmax[i+1],height[i]);
@@ -27,6 +30,7 @@ public:
 
         int result=0;
         for(int i=0; i<n; i++){
+            //result me leftmax and rightmax at index i -height of the building
             result+=min(leftmax[i],rightmax[i])-height[i];
         }
         return result;
